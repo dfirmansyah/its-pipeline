625fa1acf8f7d10f9cf8e13f74c04dd5ee079770
DIC <dicky.firmansyah@infoglobal.co.id>
2026-03-31 08:34:13 +0700

    Remove It_Port Mode attribute

--- include/pipeline/it_port.h
+++ include/pipeline/it_port.h
@@ -35,54 +35,26 @@ public:
 
 protected:
   ItPortDirection direction;
-  ItPortMode mode;
 
   PushFunc pushFunc;
   
   virtual void handleStateChanged() override {}
 
-  // virtual bool pushFunc(T data) = 0;
-  // virtual bool getDataRangeFunc(ItBuffer<T> *buffer, int length) = 0;
-  // virtual bool getDataRangeFunc(T *data) = 0;
-  virtual bool getDataRangeFunc(ItBuffer<T> *buffer, int length) { return false; };
-  virtual bool getDataRangeFunc(T *data) { return false; };
-
 public:
-  ItPort(std::string portName, ItPortDirection portDirection, ItPortMode portMode)
-      : ItObject(portName), direction(portDirection), mode(portMode) {}
   ItPort(std::string portName, ItPortDirection portDirection)
-      : ItObject(portName), direction(portDirection), mode(IT_MODE_PUSH) {}
+      : ItObject(portName), direction(portDirection) {}
   ~ItPort() = default;;
 
   ItPortDirection getDirection() const { return direction; }
-  ItPortMode getMode() const { return mode; }
 
   void push(std::unique_ptr<T> data)
   {
-  //   if (mode != IT_MODE_PUSH)
-  //     return;
-    // bool pushSuccess = pushFunc(data);
-    
     if (getState() == IT_STATE_STARTED)
     {
       pushFunc(move(data));
     }
   }
 
-  bool getDataRange(ItBuffer<T> *buffer, int length)
-  {
-    if (mode != IT_MODE_PULL)
-      return false;
-    return getDataRangeFunc(buffer, length);
-  }
-
-  bool getDataRange(T *data)
-  {
-    if (mode != IT_MODE_PULL)
-      return false;
-    return getDataRangeFunc(data);
-  }
-  
 };
 
 template <typename T>
@@ -95,28 +67,16 @@ protected:
   ItPort<T> *upstreamPort = nullptr;
   DataReceiveHandler dataReceiveHandler;
 
-  void initializePort()
-  {
-    // if (this->mode == IT_MODE_PUSH) this->pushFunc = [this](std::unique_ptr<T> d) { dataReceiveHandler(move(d)); };
-    if (this->mode == IT_MODE_PUSH) {
-      this->pushFunc = [this](std::unique_ptr<T> d) { dataReceiveHandler(move(d));};
-    }
-    else this->pushFunc = [this](std::unique_ptr<T> d) {
-      // TODO: unimplemented
-    };
-  }
-
 public:
-  ItInputPort(std::string name, ItPortMode portMode) : ItPort<T>(name, IT_DIR_INPUT, portMode) { }
   ItInputPort(std::string name) : ItPort<T>(name, IT_DIR_INPUT) { }
 
-  void setUpstreamPort(ItPort<T> *upstream_port) { upstreamPort = upstream_port; }
   ItPort<T> *getUpstreamPort() { return upstreamPort; }
+  void setUpstreamPort(ItPort<T> *upstream_port) { upstreamPort = upstream_port; }
 
   void setDataReceiveHandler(DataReceiveHandler handler)
   {
     dataReceiveHandler = handler;
-    initializePort();
+    this->pushFunc = [this](std::unique_ptr<T> d) { dataReceiveHandler(move(d));};
   }
 };
 
@@ -125,18 +85,11 @@ template <typename T>
 class ItOutputPort : public ItPort<T>
 {
 private:
-  ThreadSafeQueue<T> internal_queue;
-
   void dropPushHandler(std::unique_ptr<T> data) {}
 
   void chainDownstreamPushHandler(std::unique_ptr<T> data)
   {
     downstreamPort->push(std::move(data));
-  }
-
-  void queuePushHandler(std::unique_ptr<T> data)
-  {
-    internal_queue.push(std::move(*data));
   }
 
   void initializePushFunc()
@@ -147,36 +100,13 @@ private:
       return;
     }
 
-    switch(downstreamPort->getMode())
-    {
-      case IT_MODE_PUSH: this->pushFunc = [this](std::unique_ptr<T> d){ chainDownstreamPushHandler(move(d)); };  break;
-      case IT_MODE_PULL: this->pushFunc = [this](std::unique_ptr<T> d){ queuePushHandler(move(d)); };  break;
-      default: this->pushFunc = [this](std::unique_ptr<T> d){ dropPushHandler(move(d)); };  break;
-    }
+    this->pushFunc = [this](std::unique_ptr<T> d){ chainDownstreamPushHandler(move(d)); };
   }
 
 protected:
   ItPort<T> *downstreamPort = nullptr;
-  // virtual bool getDataRangeFunc(ItBuffer<T> *buffer, int length) = 0;
-  // virtual bool getDataRangeFunc(T *data) = 0;
-  virtual bool getDataRangeFunc(ItBuffer<T> *buffer, int length) override { return false; };
-  virtual bool getDataRangeFunc(T *data) override {
-    if (downstreamPort == nullptr || downstreamPort->getMode() != IT_MODE_PULL) return false;
-
-    if (data != nullptr)
-    {
-      internal_queue.wait_and_pop(*data);
-      return true;
-    }
-    return false;
-  };
 
 public:
-  ItOutputPort(std::string name, ItPortMode portMode) : ItPort<T>(name, IT_DIR_OUTPUT, portMode)
-  {
-    initializePushFunc();
-  }
-
   ItOutputPort(std::string name) : ItPort<T>(name, IT_DIR_OUTPUT)
   {
     initializePushFunc();
